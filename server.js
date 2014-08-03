04f412d9d7f473dc4f8e6cd2fe724e2dc2b975bf
Lucas <tartalario@yahoo.com.ar>
2014-08-03 00:26:12 -0300

    ready to deploy

--- server.js
+++ server.js
@@ -1,17 +1,58 @@
-var express = require('express');
+var express = require('express'),
+	stylus = require('stylus'),
+	mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var app = express();
+
+function compile(str, path){
+	return stylus(str).set('filename', path);
+}
 
 app.configure(function() {
 	app.set('views', __dirname + '/server/views');
 	app.set('view engine', 'jade');
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(stylus.middleware(
+		{
+			src: __dirname + '/public',
+			compile: compile
+		}
+	));
+	app.use(express.static(__dirname + '/public'));
 });
+
+if(env === 'development'){
+	mongoose.connect('mongodb://localhost/multiv');	
+} else {
+	mongoose.connect('mongodb://lucasai:lucasai2014@ds061199.mongolab.com:61199/multiv');
+}
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback(){
+	console.log('multiv	 db opened');
+});
+
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+	mongoMessage = messageDoc.message;
+});
+
+app.get('/partial/:partialPath', function(req, res){
+	res.render('partials/'+req.params.partialPath);
+})
 
 app.get('*', function(req, res){
-	res.render('index');
+	res.render('index', {
+		mongoMessage: mongoMessage
+	});
 });
 
-var port = 3030;
+var port = process.env.PORT || 3030;
 app.listen(port);
 console.log("Listening port"+ port + "...");
